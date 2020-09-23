9bdf357537c03192be109700c045159ed86cad9f
dmihi-tmihi <hidededededem@gmail.com>
2020-09-23 20:19:20 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'liberty_titles/index'
-  get 'themes/index'
   root to: "toppages#index"
+  get 'themes/index'
+  resources :liberty_titles, only: [:index, :new, :create, :show, :destoy]
 end
