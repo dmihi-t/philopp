fd824f46325a4ab8d1ac253a4e0ff8260a3fe469
dmihi-tmihi <hidededededem@gmail.com>
2020-09-23 09:52:58 +0900

    deviseビューファイルの設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'liberty_titles/index'
-  get 'titles/index'
   get 'themes/index'
-  get 'toppages/index'
   root to: "toppages#index"
 end
